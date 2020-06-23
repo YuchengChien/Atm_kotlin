6cc09ebde16d11d024f8075bd3e6825a59590b85
jason.chien <Jason_Chien@quantatw.com>
2020-06-23 19:35:47 +0800

    Create MaterialActivity

--- app/src/main/java/com/jason/atm_kotlin/MainActivity.kt
+++ app/src/main/java/com/jason/atm_kotlin/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        startActivity(Intent(this, MaterialActivity::class.java))
         if(!login) {
             // open LoginActivity
             Intent(this, LoginActivity::class.java).apply {
