379a268fe3dbfd389c9345c72f5c7825d7a99860
jason.chien <Jason_Chien@quantatw.com>
2020-06-23 17:34:55 +0800

    Add LoginActivity

--- app/src/main/java/com/jason/atm_kotlin/LoginActivity.kt
+++ app/src/main/java/com/jason/atm_kotlin/LoginActivity.kt
@@ -2,15 +2,20 @@ package com.jason.atm_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
-    var login = false;
+class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if(!login) {
-            // open LoginActivity
+        setContentView(R.layout.activity_login)
+    }
 
-        }
+    fun login(view: View) {
+
+    }
+
+    fun cancel(view: View) {
+
     }
 }
